0e5f1cc2a95c31efb9ea68192c2c9bd031f0928a
RicciardiRiccardo <117997389+RicciardiRiccardo@users.noreply.github.com>
2022-12-01 19:53:57 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,17 +1,19 @@
 #include <iostream>
-using namespace std;
 #include "lib.h"
 
-int main() {
+using namespace std;
+
+int main()
+{
     int n;
     cin >> n;
-    bool risultato = funzione(n);
-    if (risultato == true){
+    if (NumeroPrimo (n,n) )
+    {
         cout << "numero primo" << endl;
     }
     else
     {
         cout << "numero non primo" << endl;
     }
-return 0;
+    return 0;
 }
